a7683a3ab7990e7a2d203ec1e3cf50b0b86ab272
denkeel <denkeelpro@gmail.com>
2020-10-07 14:01:54 +0300

    Добавил простенькие тесты

---
+++ tests/Feature/AddTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class NewsAddTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $response = $this->get('/add');
+
+        $response->assertSee('Добавить заказ');
+        $response->assertStatus(200);
+    }
+}
